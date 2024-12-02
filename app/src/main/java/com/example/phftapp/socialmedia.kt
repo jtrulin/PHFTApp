3fd3353aee84e5468d9da204f8c43721ab66a7b7
jaimef7 <jxf8016@mavs.uta.edu>
2024-12-01 21:10:09 -0600

    now??

--- app/src/main/java/com/example/phftapp/socialmedia.kt
+++ app/src/main/java/com/example/phftapp/socialmedia.kt
@@ -11,7 +11,7 @@ class socialmedia : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_socialmedia)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.socialmediaID)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
